53b4775a0e70d2f8ee861bce211d0132975bccf3
林田真生子 <hayashidamakiko@hayashidamakikonoMacBook-Pro.local>
2023-01-25 23:52:01 +0900

    vuetifyインストールした

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,55 +1,18 @@
 <template>
-  <div class="home"></div>
-  <!--  {{titles[1].Title}}-->
-  ---------------------------------------------------------------------------------
-  <div v-for=" title in titles" :key="title">タイトル→{{ title.Title }}<br>
-  <br>コメント→{{ title.Content }}<br>
-  ---------------------------------------------------------------------------------
-  </div>
-
+  <HelloWorld />
 </template>
 
-<style scoped>
-
-
-</style>
-
 <script>
-export default {
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
   name: 'HomeView',
-  data() {
-    return {
-      titles: "unko",
-      contents: "vvvv",
-      button: "unko",
-    }
+
+  components: {
+    HelloWorld,
   },
-  /*methods: {
-    click_count: function () {
-      console.log("ZZZZZZZZZZZZZZZZZZZZZZ",this.messge)
-      this.countunko++;
-      console.log("sssssssss",this.countunko)
-      this.button = "manko"
-
-    },
-  },
-*/
-
-  async mounted() {
-    //let title = "a"
-    //this.button = title
-    const data = await fetch("http://localhost:8080/hello")
-    const json = await data.json()
-    //const json = ["hoge", "fuga", "piyo"];
-    this.titles = json.title
-    //this.contents = json.content
-    console.log("oooooooooooo", this.titles)
-    //console.log("oooooooooooo",)
-
-
-  },
-
-
-}
-
+});
 </script>
