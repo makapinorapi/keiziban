76720530c6772f5d8b2cc86462938cd9ba8f7ab3
林田真生子 <hayashidamakiko@hayashidamakikonoMacBook-Pro.local>
2023-01-27 22:46:47 +0900

    コメントフォーム保存

--- src/views/KomentForm.vue
+++ src/views/KomentForm.vue
@@ -1,12 +1,68 @@
 <template>
-  <h2>つぶやこう！</h2>
-  <div>タイトル<input type="text" v-model="title"></div>
-  <div class="comment">コメント</div>
-  <textarea v-model="content"></textarea>
-  <div class="center">
-    <button @click="save">投稿する</button>
-  </div>
-
+    <v-card
+        class="overflow-hidden"
+        color="white"
+        dark
+    >
+      <v-toolbar
+          flat
+          color="white"
+      >
+        <v-icon>mdi-account</v-icon>
+        <v-toolbar-title class="font-weight-light">
+          User Profile
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn
+            color="purple darken-3"
+            fab
+            small
+            @click="isEditing = !isEditing"
+        >
+          <v-icon v-if="isEditing">
+            mdi-close
+          </v-icon>
+          <v-icon v-else>
+            mdi-pencil
+          </v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-text>
+        <v-text-field
+            :disabled="!isEditing"
+            color="white"
+            label="Name"
+        ></v-text-field>
+        <v-autocomplete
+            :disabled="!isEditing"
+            :items="states"
+            :filter="customFilter"
+            color="white"
+            item-text="name"
+            label="State"
+        ></v-autocomplete>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            :disabled="!isEditing"
+            color="success"
+            @click="save"
+        >
+          Save
+        </v-btn>
+      </v-card-actions>
+      <v-snackbar
+          v-model="hasSaved"
+          :timeout="2000"
+          absolute
+          bottom
+          left
+      >
+        Your profile has been updated
+      </v-snackbar>
+    </v-card>
 </template>
 
 <script>
